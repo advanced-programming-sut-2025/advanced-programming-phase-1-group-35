d6f328f96b417f5f2fc24b67da1c5be9f2ab5a43
Hosein0withg <hoseinbakhoda2006@gmail.com>
2025-04-09 01:51:06 +0330

    just empty classes and methods f or uml and phase 0

---
+++ src/main/java/Model/Weather.java
@@ -0,0 +1,27 @@
+package Model;
+
+import Model.Enum.WeatherCondition;
+
+public class Weather {
+    private WeatherCondition weatherCondition;
+
+    public void applySunnyWeather() {
+
+    }
+
+    public void applyRainWeather() {
+
+    }
+
+    public void applySnowWeather() {
+
+    }
+
+    public void applyStormWeather() {
+
+    }
+
+    public void applyLightning() {
+
+    }
+}
