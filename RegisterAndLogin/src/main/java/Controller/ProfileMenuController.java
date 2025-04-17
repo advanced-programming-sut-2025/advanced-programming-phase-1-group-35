0c621173b5cf06ee9c4d44eebd399cfa5b2b8d16
mhdsdg <mhdsdg4@gmail.com>
2025-04-17 16:23:03 +0330

    profile menu added

--- RegisterAndLogin/src/main/java/Controller/ProfileMenuController.java
+++ RegisterAndLogin/src/main/java/Controller/ProfileMenuController.java
@@ -1,27 +1,64 @@
 package Controller;
 
+import Model.App;
 import Model.Result;
+import Model.User;
+import Model.enums.Regexes;
 
 import java.security.PublicKey;
+import java.util.Scanner;
 
 public class ProfileMenuController {
-    public Result showUserInfo() {
-        return null;
+    LoginMenuController loginMenuController = new LoginMenuController();
+    public void showUserInfo() {
+        User user = App.getLoggedInUser();
+        System.out.println("Username : " + user.getUsername() +
+                "\nNickname : " + user.getNickname() +
+                "\nHighScore : " + user.getHighScore() +
+                "\nGames Played : " + user.getGamesPlayed());
     }
 
     public Result changeUsername(String username) {
-        return null;
+        if(username.equals(App.getLoggedInUser().getUsername())) {
+            return new Result(false , "now that wouldn't be a change would it ?");
+        }
+        if(Regexes.Username.getMatcher(username) == null){
+            return new Result(false , "new username is invalid");
+        }
+        if(loginMenuController.getUser(username) != null){
+            return new Result(false , "username is already taken");
+        }
+        App.getLoggedInUser().setUsername(username);
+        return new Result(true , "username has been changed");
     }
 
-    public Result changePassword(String oldPassword, String newPassword) {
-        return null;
+    public Result changePassword(String oldPassword, String newPassword , Scanner scanner) {
+        if(oldPassword.equals(newPassword)){
+            return new Result(false , "now that wouldn't be a change would it ?");
+        }
+        Result managePasswordResult = loginMenuController.managePassword(newPassword , newPassword , scanner);
+        if(!managePasswordResult.isSuccess()) return managePasswordResult;
+        else newPassword = managePasswordResult.toString();
+        App.getLoggedInUser().setPassword(newPassword);
+        return new Result(true , "password has been changed");
     }
 
     public Result changeNickname(String nickname) {
-        return null;
+        if(nickname.equals(App.getLoggedInUser().getNickname())){
+            return new Result(false , "now that wouldn't be a change would it ?");
+        }
+        App.getLoggedInUser().setNickname(nickname);
+        return new Result(true , "nickname has been changed");
     }
 
     public Result changeEmail(String email) {
-        return null;
+        if(email.equals(App.getLoggedInUser().getEmail())){
+            return new Result(false , "now that wouldn't be a change would it ?");
+        }
+        if(Regexes.Email.getMatcher(email) == null){
+            return new Result(false , "new email is invalid");
+        }
+        App.getLoggedInUser().setEmail(email);
+        return new Result(true , "email has been changed");
     }
 }
