992b1e289e11b16465e8015814c7d14857e95e83
Mahdi <msrjk1384@gmail.com>
2025-04-25 01:50:08 +0330

    slight change in ForagingSeeds

--- src/Model/enums/Crops/ForagingSeeds.java
+++ src/Model/enums/Crops/ForagingSeeds.java
@@ -5,54 +5,56 @@ import Model.enums.Seasons;
 import java.util.Arrays;
 import java.util.List;
 
+import static Model.enums.Crops.Crop.*;
 import static Model.enums.Seasons.*;
 
 public enum ForagingSeeds implements PlantAble {
-    JAZZ(Arrays.asList(Spring)),
-    CARROT(Arrays.asList(Spring)),
-    CAULIFLOWER(Arrays.asList(Spring)),
-    COFFEE(Arrays.asList(Spring)),
-    GARLIC(Arrays.asList(Spring)),
-    BEAN(Arrays.asList(Spring)),
-    KALE(Arrays.asList(Spring)),
-    PARSNIP(Arrays.asList(Spring)),
-    POTATO(Arrays.asList(Spring)),
-    RHUBARB(Arrays.asList(Spring)),
-    STRAWBERRY(Arrays.asList(Spring)),
-    TULIP(Arrays.asList(Spring)),
-    RICE(Arrays.asList(Spring)),
-    BLUEBERRY(Arrays.asList(Summer)),
-    CORN(Arrays.asList(Summer)),
-    HOPS(Arrays.asList(Summer)),
-    PEPPER(Arrays.asList(Summer)),
-    MELON(Arrays.asList(Summer)),
-    POPPY(Arrays.asList(Summer)),
-    RADISH(Arrays.asList(Summer)),
-    RED_CABBAGE(Arrays.asList(Summer)),
-    STARFRUIT(Arrays.asList(Summer)),
-    SPANGLE(Arrays.asList(Summer)),
-    SUMMER_SQUASH(Arrays.asList(Summer)),
-    SUNFLOWER(Arrays.asList(Summer)),
-    TOMATO(Arrays.asList(Summer)),
-    WHEAT(Arrays.asList(Summer)),
-    AMARANTH(Arrays.asList(Fall)),
-    ARTICHOKE(Arrays.asList(Fall)),
-    BEET(Arrays.asList(Fall)),
-    BOK_CHOY(Arrays.asList(Fall)),
-    BROCCOLI(Arrays.asList(Fall)),
-    CRANBERRY(Arrays.asList(Fall)),
-    EGGPLANT(Arrays.asList(Fall)),
-    FAIRY(Arrays.asList(Fall)),
-    GRAPE(Arrays.asList(Fall)),
-    PUMPKIN(Arrays.asList(Fall)),
-    YAM(Arrays.asList(Fall)),
-    RARE(Arrays.asList(Fall)),
-    POWDERMELON(Arrays.asList(Winter)),
-    ANCIENT(Arrays.asList(Spring, Summer, Fall, Winter)),
-    MIXED(Arrays.asList(Spring, Summer, Fall, Winter));
+    JAZZ(Arrays.asList(Spring), Crop.BLUE_JAZZ),
+    CARROT(Arrays.asList(Spring), Crop.CARROT),
+    CAULIFLOWER(Arrays.asList(Spring), Crop.CAULIFLOWER),
+    COFFEE(Arrays.asList(Spring), Crop.COFFEE_BEAN),
+    GARLIC(Arrays.asList(Spring), Crop.GARLIC),
+    BEAN(Arrays.asList(Spring), Crop.GREEN_BEAN),
+    KALE(Arrays.asList(Spring), Crop.KALE),
+    PARSNIP(Arrays.asList(Spring), Crop.PARSNIP),
+    POTATO(Arrays.asList(Spring), Crop.POTATO),
+    RHUBARB(Arrays.asList(Spring), Crop.RHUBARB),
+    STRAWBERRY(Arrays.asList(Spring), Crop.STRAWBERRY),
+    TULIP(Arrays.asList(Spring), Crop.TULIP),
+    RICE(Arrays.asList(Spring), UNMILLED_RICE),
+    BLUEBERRY(Arrays.asList(Summer), Crop.BLUEBERRY),
+    CORN(Arrays.asList(Summer), Crop.CORN),
+    HOPS(Arrays.asList(Summer), Crop.HOPS),
+    PEPPER(Arrays.asList(Summer), HOT_PEPPER),
+    MELON(Arrays.asList(Summer), Crop.MELON),
+    POPPY(Arrays.asList(Summer), Crop.POPPY),
+    RADISH(Arrays.asList(Summer), Crop.RADISH),
+    RED_CABBAGE(Arrays.asList(Summer), Crop.RED_CABBAGE),
+    STARFRUIT(Arrays.asList(Summer), Crop.STARFRUIT),
+    SPANGLE(Arrays.asList(Summer), SUMMER_SPANGLE),
+    SUMMER_SQUASH(Arrays.asList(Summer), Crop.SUMMER_SQUASH),
+    SUNFLOWER(Arrays.asList(Summer), Crop.SUNFLOWER),
+    TOMATO(Arrays.asList(Summer), Crop.TOMATO),
+    WHEAT(Arrays.asList(Summer), Crop.WHEAT),
+    AMARANTH(Arrays.asList(Fall), Crop.AMARANTH),
+    ARTICHOKE(Arrays.asList(Fall), Crop.ARTICHOKE),
+    BEET(Arrays.asList(Fall), Crop.BEET),
+    BOK_CHOY(Arrays.asList(Fall), Crop.BOK_CHOY),
+    BROCCOLI(Arrays.asList(Fall), Crop.BROCCOLI),
+    CRANBERRY(Arrays.asList(Fall), CRANBERRIES),
+    EGGPLANT(Arrays.asList(Fall), Crop.EGGPLANT),
+    FAIRY(Arrays.asList(Fall), FAIRY_ROSE),
+    GRAPE(Arrays.asList(Fall), Crop.GRAPE),
+    PUMPKIN(Arrays.asList(Fall), Crop.PUMPKIN),
+    YAM(Arrays.asList(Fall), Crop.YAM),
+    RARE(Arrays.asList(Fall), Crop.SWEET_GEM_BERRY),
+    POWDERMELON(Arrays.asList(Winter), Crop.POWDERMELON),
+    ANCIENT(Arrays.asList(Spring, Summer, Fall, Winter), null),
+    MIXED(Arrays.asList(Spring, Summer, Fall, Winter), null);
 
     private final List<Seasons> seasons;
     private final Crop crop;
+
     ForagingSeeds(List<Seasons> seasons, Crop crop) {
         this.seasons = seasons;
         this.crop = crop;
@@ -60,6 +62,10 @@ public enum ForagingSeeds implements PlantAble {
 
     public List<Seasons> getSeasons() {
         return seasons;
+    }
+
+    public Crop getCrop() {
+        return crop;
     }
 
 }
