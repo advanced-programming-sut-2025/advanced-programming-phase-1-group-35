a47dff8f95f15d40368d475f7ca0c47f2e8cd192
Mahdi <msrjk1384@gmail.com>
2025-05-11 12:03:33 +0330

    normal commit

--- RegisterAndLogin/src/main/java/Controller/InGameMenu/CraftingController.java
+++ RegisterAndLogin/src/main/java/Controller/InGameMenu/CraftingController.java
@@ -1,5 +1,6 @@
 package Controller.InGameMenu;
 
+import Model.App;
 import Model.Tile;
 import Model.ItemInterface;
 import Model.enums.CraftingItems;
@@ -8,7 +9,39 @@ import java.util.List;
 
 public class CraftingController {
     private List<CraftingItems> showRecepies(){return null;}
-    private ItemInterface craftItem(String itemName){return null;}
+    private ItemInterface craftItem(String itemName, List<String> Ingredients){
+        ItemInterface item = null;
+        for (CraftingItems craftingItem : CraftingItems.values()) {
+            if (craftingItem.name().equals(itemName)) {
+                item = craftingItem;
+                break;
+            }
+        }
+        for(String ingredient : Ingredients){
+    ItemInterface IngredientItem = null;
+            for (CraftingItems craftingItem : CraftingItems.values()) {
+                if (craftingItem.name().equals(ingredient)) {
+                    IngredientItem = craftingItem;
+                    break;
+                }
+            }
+            if (IngredientItem == null) {
+                System.out.println("Ingredient " + ingredient + " not found");
+                return null;
+            }
+            if(item == null){
+                System.out.println("the given item is not found");
+                return null;
+            }
+            if(!App.getCurrentGame().getPlayingUser().getInventory().itemInterfaces.contains(IngredientItem)){
+                System.out.println("you don't have " + IngredientItem.getName() + " in your inventory");
+                return null;
+            }
+            if()
+        }
+
+
+    }
     public void addRecipe(CraftingItems recipe){}
     public boolean doesInventoryHasSpace(){return true;}
     public void placeItem(String ItemName, Tile tile){}
