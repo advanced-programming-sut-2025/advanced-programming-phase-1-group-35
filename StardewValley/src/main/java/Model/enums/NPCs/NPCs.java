e8b5f885ae3df6be604b7845062e207488960887
Hosein0withg <hoseinbakhoda2006@gmail.com>
2025-05-16 11:37:58 +0330

    fix a npc bug

--- StardewValley/src/main/java/Model/enums/NPCs/NPCs.java
+++ StardewValley/src/main/java/Model/enums/NPCs/NPCs.java
@@ -19,50 +19,63 @@ public enum NPCs {
     Harvey,
     Lia,
     //shop and quest
-    Robin,
-    ;
-    public String name;
-    public Quest[] quests;
-    public String job;
+    Robin;
 
     public NPC createNPC() {
-        if (name.equals(Sebastian.name) || name.equals(Abigail.name)
-                || name.equals(Harvey.name) || name.equals(Lia.name) || name.equals(Robin.name)) {
-            return new NPC(name, job, quests(), favorites());
+        if (hasQuests()) {
+            return new NPC(this.name(), getJob(), quests(), favorites());
         } else {
-            return new NPC(name, job, new ArrayList<Quest>(), new ArrayList<String>());
+            return new NPC(this.name(), getJob(), new ArrayList<>(), new ArrayList<>());
         }
+    }
+
+    private boolean hasQuests() {
+        return switch (this) {
+            case Sebastian, Abigail, Harvey, Lia, Robin -> true;
+            default -> false;
+        };
+    }
+
+    private String getJob() {
+        // You can assign jobs here if needed
+        return switch (this) {
+            case Clint -> "Blacksmith";
+            case Morris -> "JojaMart Manager";
+            case Pierre -> "Shopkeeper";
+            case Willy -> "Fisherman";
+            case Marnie -> "Rancher";
+            case Gus -> "Bartender";
+            default -> "Villager";
+        };
     }
 
     private ArrayList<String> favorites() {
         ArrayList<String> favorites = new ArrayList<>();
-        switch (this.name()) {
-            case "Sebastian":
+        switch (this) {
+            case Sebastian:
                 favorites.add("Wool");
                 favorites.add("Pumpkin Pie");
                 favorites.add("Pizza");
                 break;
-            case "Abigail":
+            case Abigail:
                 favorites.add("Rock");
                 favorites.add("Iron Ore");
                 favorites.add("Coffee");
-            case "Harvey":
+                break;
+            case Harvey:
                 favorites.add("Coffee");
                 favorites.add("Pickles");
                 favorites.add("Wine");
                 break;
-            case "Lia":
+            case Lia:
                 favorites.add("Salad");
                 favorites.add("Grape");
                 favorites.add("Wine");
                 break;
-            case "Robin":
+            case Robin:
                 favorites.add("Spaghetti");
                 favorites.add("Wood");
                 favorites.add("Iron Ingot");
-                break;
-            default:
-                favorites.add("");
                 break;
         }
         return favorites;
@@ -70,33 +83,31 @@ public enum NPCs {
 
     private ArrayList<Quest> quests() {
         ArrayList<Quest> quests = new ArrayList<>();
-        switch (this.name()) {
-            case "Sebastian":
+        switch (this) {
+            case Sebastian:
                 quests.add(new Quest("50 units of iron", "2 diamonds"));
-                quests.add(new Quest("pumpkin pie", "5000 gold coins"));
+                quests.add(new Quest("Pumpkin Pie", "5000 gold coins"));
                 quests.add(new Quest("150 units of stone", "50 quartz"));
                 break;
-            case "Abigail":
-                quests.add(new Quest("a gold bar", "1 friendship level"));
-                quests.add(new Quest("a pumpkin", "500 gold coins"));
+            case Abigail:
+                quests.add(new Quest("1 gold bar", "1 friendship level"));
+                quests.add(new Quest("1 pumpkin", "500 gold coins"));
                 quests.add(new Quest("50 wheat", "1 automatic iridium sprinkler"));
                 break;
-            case "Harvey":
+            case Harvey:
                 quests.add(new Quest("12 of any plant", "750 gold coins"));
-                quests.add(new Quest("a salmon fish", "1 friendship level"));
-                quests.add(new Quest("a bottle of wine", "5 salads"));
+                quests.add(new Quest("1 salmon", "1 friendship level"));
+                quests.add(new Quest("1 bottle of wine", "5 salads"));
                 break;
-            case "Lia":
+            case Lia:
                 quests.add(new Quest("10 hardwood", "500 gold coins"));
-                quests.add(new Quest("a salmon fish", "Salmon dinner cooking recipe"));
-                quests.add(new Quest("200 wood", "3 deluxe scarecrow"));
+                quests.add(new Quest("1 salmon", "Salmon dinner recipe"));
+                quests.add(new Quest("200 wood", "3 deluxe scarecrows"));
                 break;
-            case "Robin":
+            case Robin:
                 quests.add(new Quest("80 wood", "1000 gold coins"));
                 quests.add(new Quest("10 iron bars", "3 beehives"));
                 quests.add(new Quest("1000 wood", "25000 gold coins"));
-                break;
-            default:
                 break;
         }
         return quests;
